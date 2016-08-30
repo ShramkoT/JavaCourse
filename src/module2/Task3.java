271a5efe2e542a598b02983b8cd63e02c4a90962
Tiger <Shramkot@ukr.net>
2016-08-30 19:46:59 +0300

    rework 1

--- src/module2/Task3.java
+++ src/module2/Task3.java
@@ -1,36 +1,29 @@
 package module2;
 
 public class Task3 {
-    static double withdrawBalance(double balance, double withdrawal, double commitionFinal) {
 
-        return balance - withdrawal - commitionFinal;
+    static double[] balances = {460, 810, 1200, 930, 1830};
+    static String[] ownerNames = {"Tirion", "Sansa", "Rob", "John", "Deineris"};
+    static double commition = 0.05;
+
+    static void withdrawBalance(String name, double fund) {
+        int n = 0;
+        for (int i = 0; i<ownerNames.length; i++) {
+            if (name == ownerNames[i]){
+                n =i;
+                break;
+            }
+        }
+
+        balances[n] = balances[n] - fund*commition - fund;
+        if (balances[n] >= 0){
+            System.out.println(ownerNames[n] + " " + fund*commition + " " + balances[n]);
+        }
+        else {
+            System.out.println("NO");
+        }
     }
-
     public static void main(String[] args) {
-        int[] balances = {460, 810, 1200, 930, 1830};
-        String[] ownerNames = {"Tirion", "Sansa", "Rob", "John", "Deineris"};
-        double withdraw = 500;
-        double commition = 0.05;
-        double commitionFinal = commition * withdraw;
-        double balanceAfter1 = withdrawBalance(balances[1], withdraw, commitionFinal);
-        double balanceAfter2 = withdrawBalance(balances[0], withdraw, commitionFinal);
-
-
-            if(balanceAfter1 >= 0) {
-                System.out.println(ownerNames[1] + " " + withdraw + " " + balanceAfter1);
-            }
-
-            else {
-                System.out.println(ownerNames[1] + " " + "NO");
-            }
-
-             if(balanceAfter2 >= 0) {
-                System.out.println(ownerNames[0] + " " + withdraw + " " + balanceAfter2);
-            }
-
-             else {
-                System.out.println(ownerNames[0] + " " + "NO");
-            }
-
+        withdrawBalance("John", 630);
     }
 }
