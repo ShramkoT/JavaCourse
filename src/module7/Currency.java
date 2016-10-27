cc7ff13f0bc4b50aef139f30f7d555e29e06c840
Tiger <Shramkot@ukr.net>
2016-10-27 21:05:30 +0300

    task 7.1

---
+++ src/module7/Currency.java
@@ -0,0 +1,6 @@
+package module7;
+
+public enum Currency {
+    USD,
+    UAH
+}
---
+++ src/module7/Order.java
@@ -0,0 +1,10 @@
+package module7;
+
+public class Order {
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+}
